2544694151fcb305e3fe27c6d6085da823f8a6c3
ishqdehlvi <inzemamhaq05@gmail.com>
2023-08-15 12:04:28 +0530

    sdgasdg

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", "http://3.209.214.84/", "https://www.fountaininstitute.in/"),
+  url: env(
+    "URL",
+    "http://3.209.214.84:1337",
+    "https://www.fountaininstitute.in/"
+  ),
   app: {
     keys: env.array("APP_KEYS"),
   },
